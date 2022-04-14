bb45e82cf39f5b071d4fa2fd1bfe3885367fbc15
Gustavo Meza De Lama <gmezadelama@gmail.com>
2022-04-14 15:41:12 -0400

    Fixing bug on Designo attributes

--- src/components/attributes/DesignoAttribute.tsx
+++ src/components/attributes/DesignoAttribute.tsx
@@ -1,11 +1,13 @@
 export interface IDesignoAttributeProps {
   imageClass: string;
+  imageSrc: string;
   title: string;
   description: string;
 }
 
 const DesignoAttribute = ({
   imageClass,
+  imageSrc,
   title,
   description,
 }: IDesignoAttributeProps) => {
@@ -16,9 +18,16 @@ const DesignoAttribute = ({
           "grid gap-y-12 gap-x-0 grid-rows-[auto_auto] sm:gap-y-0 sm:gap-x-12 md:gap-y-12 md:gap-x-0 sm:grid-cols-[auto_1fr] md:grid-rows-[auto_auto] md:grid-cols-none"
         }
       >
-        <div
-          className={`${imageClass} bg-cover bg-no-repeat w-[202px] h-[202px] inline-block mx-auto`}
-        />
+        <div className={"w-[202px] h-[202px] relative mx-auto"}>
+          <div
+            className={`${imageClass} bg-cover bg-no-repeat w-full h-full inline-block mx-auto`}
+          ></div>
+          <img
+            src={imageSrc}
+            className={"w-full h-full absolute top-0"}
+            alt={title}
+          />
+        </div>
         <div className={"grid gap-y-8 sm:gap-y-4 md:gap-y-8"}>
           <h3
             className={"text-center sm:text-left md:text-center mt-auto mb-0"}
--- src/components/attributes/DesignoAttributesList.tsx
+++ src/components/attributes/DesignoAttributesList.tsx
@@ -10,6 +10,7 @@ const DesignoAttributesList = () => {
             "Each project starts with an in-depth brand research to ensure we only create products that server a purpose. We merge art, design, and technology into exciting new solution."
           }
           imageClass={"attribute-passionate"}
+          imageSrc={"/assets/home/desktop/illustration-passionate.svg"}
         />
         <DesignoAttribute
           title={"Resourceful"}
@@ -17,6 +18,7 @@ const DesignoAttributesList = () => {
             "Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients' needs."
           }
           imageClass={"attribute-resourceful"}
+          imageSrc={"/assets/home/desktop/illustration-resourceful.svg"}
         />
         <DesignoAttribute
           title={"Friendly"}
@@ -24,6 +26,7 @@ const DesignoAttributesList = () => {
             "We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide."
           }
           imageClass={"attribute-friendly"}
+          imageSrc={"/assets/home/desktop/illustration-friendly.svg"}
         />
       </div>
     </>
